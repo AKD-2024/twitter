17bdb4113033af033022afb3060cc6cbb8d351d3
Anjali <anjali.dubey@comprotechnologies.com>
2024-10-22 17:03:19 +0530

    Learnt logging out users, login form, accessing the current user, and edit password

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "about", to: "about#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update", as: :update_password
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
-  get "about", to: "about#index"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   # get "/", to: "main#index" - root
   root "main#index"
 end
